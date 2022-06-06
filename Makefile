45146956e22634a7129a672794d9da6a25a71460
Gabriel Torello <gabriel128@gmail.com>
2022-06-06 20:43:53 +1000

    Read basic kernel

--- Makefile
+++ Makefile
@@ -1,13 +1,27 @@
 all: build run
 
-build:
+build: clean
+	nasm src/bootloader/boot.asm -f bin -o bin/boot.bin
+	dd if=./bin/boot.bin >> ./bin/kernel.img
+	truncate --size 1M ./bin/kernel.img
+
+run:
+	qemu-system-x86_64 bin/kernel.img
+
+c-kernel: clean
+	gcc -ffreestanding -c ./src/c_kernel/kernel.c -o ./bin/c_kernel/kernel.o
+	ld -o ./bin/c_kernel/kernel.bin -Ttext 0x9000 ./bin/c_kernel/kernel.o --oformat binary
 	nasm src/bootloader/boot.asm -f bin -o bin/boot.bin
 
-run: 
-	qemu-system-x86_64 bin/boot.bin
+	dd if=./bin/boot.bin >> ./bin/kernel.img
+	dd if=./bin/c_kernel/kernel.bin >> ./bin/kernel.img
+	truncate --size 1M ./bin/kernel.img
 
 clean:
-	rm -rf bin/boot.bin
+	rm -rf bin/boot.bing
+	rm -rf bin/kernel.img
+	rm -rf bin/c_kernel/*.o
+	rm -rf bin/c_kernel/*.bin
 
 debug: build
 	gdb -q
--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,20 +1,36 @@
-ORG 0x7c00
+[ORG 0x7c00]
 BITS 16
 
+stack:
+
+KERNEL_OFFSET equ 0x9000
+;; Just in case dl gets overriden
+
 setup_real_mode:
-    cli                         ; Clear Interrupts
+    mov [BOOT_DRIVE], dl
+    cli ; Clear Interrupts
     mov ax, 0x00
     mov ds, ax
     mov es, ax
     mov ss, ax
-    ;; Setup stack
     mov ebp, 0x8000
-    mov esp, ebp
-    sti                         ; Enble interrupts back
+    mov sp, bp
+    sti ; Enables Interrupts
 
 start_real_mode:
     mov si, REAL_MODE_START_MSG
     call rm_print_string
+
+    ;; Load kernel
+    mov al, 125                 ; Loads 64KB to not break the DMA boundaries
+    mov cl, 2                   ; Start reading from the second sector
+    mov bx, KERNEL_OFFSET
+    mov dl, [BOOT_DRIVE]
+
+    call load_kernel
+
+    ;; mov si, KERNEL_OFFSET
+    ;; call rm_print_string
 
     mov si, PROTECTED_MODE_START_MSG
     call rm_print_string
@@ -26,11 +42,13 @@ start_real_mode:
 
 imports_real_mode:
     %include "src/bootloader/utils/debug_print.asm"
+    %include "src/bootloader/utils/disk.asm"
     %include "src/bootloader/switch_pmode.asm"
 
 [BITS 32]
 
 after_pmode_switch:
+    jmp CODE_SEG:KERNEL_OFFSET
     jmp end_prog
 
 end_prog:
@@ -41,8 +59,9 @@ end_prog:
 [BITS 16]
 
 ;; Declare consts
-REAL_MODE_START_MSG:  db 10, "Starting real mode (iow things are getting real). ", 0
-PROTECTED_MODE_START_MSG: db "Starting protected mode. ", 0
+BOOT_DRIVE db 0
+REAL_MODE_START_MSG:  db 10, "> Starting real mode (iow things are getting real). ", 0
+PROTECTED_MODE_START_MSG: db "> Starting protected mode. ", 0
 
 
 ;; Set magic number
